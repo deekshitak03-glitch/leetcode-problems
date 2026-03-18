119cdcdb5152b3849c6ba1627fe22bd84619b478
deekshitak03-glitch <deekshitak03@gmail.com>
2026-03-18 10:08:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (9.61%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.c
@@ -0,0 +1,18 @@
+bool checkPerfectNumber(int n) {
+    int sum=1;
+    if(n==1) return false;
+    for(int i=2;i<=n/i;i++){
+        if(n%i==0){
+            if(i==n/i)
+              sum=sum+i;
+            else
+            sum=sum+i+(n/i);
+        }
+    }
+    if(n==1)
+     sum=0;
+    if(sum==n)
+      return true;
+    else
+      return false;
+}
