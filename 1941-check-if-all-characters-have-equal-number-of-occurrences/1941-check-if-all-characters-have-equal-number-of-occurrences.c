29be6589bcca45917652c8b0b1f4f85a90ed140f
deekshitak03-glitch <deekshitak03@gmail.com>
2026-03-30 12:41:32 +0530

    Time: 0 ms (100%), Space: 8.8 MB (51.28%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.c
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.c
@@ -1,25 +1,14 @@
 bool areOccurrencesEqual(char* s) {
-  
-    int freq[26] = {0};
-
-    // Count frequency of each character
-    for (int i = 0; s[i] != '\0'; i++) {
-        freq[s[i] - 'a']++;
-    }
-
-    int expected = 0;
-
-    // Check all frequencies are same
-    for (int i = 0; i < 26; i++) {
-        if (freq[i] != 0) {
-            if (expected == 0) {
-                expected = freq[i];  // first character frequency
-            } else if (freq[i] != expected) {
-                return false;
-            }
-        }
-    }
-
-    return true;
-
+ int* f=calloc(26,sizeof(int));
+ int n=strlen(s);
+ for(int i=0;i<n;i++){
+      int val=s[i]-'a';
+      f[val]++;
+ }
+ int ans = f[s[0]-'a'];
+ for(int j=0;j<26;j++){
+    if(f[j]==0)  continue;
+    if (f[j] != ans) return false;
+ }
+ return true;
 }
