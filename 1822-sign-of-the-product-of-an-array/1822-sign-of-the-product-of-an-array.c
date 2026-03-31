1183ee8ae4238d044a74598d9d4c2af36428339a
deekshitak03-glitch <deekshitak03@gmail.com>
2026-03-31 21:46:13 +0530

    Time: 0 ms (100%), Space: 9.2 MB (13.24%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.c
@@ -0,0 +1,13 @@
+int arraySign(int* nums, int numsSize) {
+    int sign = 1;
+
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == 0)
+            return 0;          // if zero → product is zero
+
+        if (nums[i] < 0)
+            sign = -sign;      // flip sign
+    }
+
+    return sign;
+}
