1795eb9c7758c2ea01fcab44addea9fe2a377e7b
deekshitak03-glitch <deekshitak03@gmail.com>
2026-04-01 10:56:32 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.17%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.c
@@ -0,0 +1,12 @@
+double average(int* salary, int salarySize) {
+    int min = salary[0], max = salary[0];
+    int sum = 0;
+
+    for(int i = 0; i < salarySize; i++) {
+        if(salary[i] < min) min = salary[i];
+        if(salary[i] > max) max = salary[i];
+        sum += salary[i];
+    }
+
+    return (double)(sum - min - max) / (salarySize - 2);
+}
