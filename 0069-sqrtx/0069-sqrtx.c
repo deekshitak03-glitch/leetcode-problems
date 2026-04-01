5ae0bab02e5f6f3173edc3b408fbcc508bf5381f
deekshitak03-glitch <deekshitak03@gmail.com>
2026-04-01 11:50:25 +0530

    Time: 0 ms (100%), Space: 9.1 MB (59.65%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.c
@@ -0,0 +1,22 @@
+int mySqrt(int x) {
+    if (x == 0 || x == 1) return x;
+
+    long left = 1, right = x, ans = 0;
+
+    while (left <= right) {
+        long mid = left + (right - left) / 2;
+
+        if (mid * mid == x) {
+            return mid;
+        } 
+        else if (mid * mid < x) {
+            ans = mid;        // store possible answer
+            left = mid + 1;
+        } 
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return ans;
+}
